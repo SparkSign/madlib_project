9925b9782d23b5f3f7348c5b26122cb17fcca21e
SparkSign <59457080+SparkSign@users.noreply.github.com>
2023-03-15 14:20:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+adj1 = input("Please enter an adjective. ")
+noun1 = input("Please enter a noun; vehicle. ")
+noun2 = input("Please enter a noun; number. ")
+noun3 = input("Please enter a plural noun; people. ")
+noun4 = input("Please enter a noun; US state. ")
+
+print(f"This summer my family is taking a {adj1} trip in our {noun1}. We are driving {noun2} miles to visit our {noun3} in the state of {noun4}")
